fa05ea87722ff646f1b073246812ecdb43937838
Jackzbb <zbb010806>
2023-04-09 23:58:16 +0800

    测试一下

---
+++ TestDemo11.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User:jack
+ * Date:2023-04-09
+ * Time:23:51
+ */
+public class TestDemo11 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ TestDemo20.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User:jack
+ * Date:2023-04-09
+ * Time:23:54
+ */
+public class TestDemo20 {
+
+
+    public static void main(String[] args) {
+        System.out.println();
+
+    }
+}
