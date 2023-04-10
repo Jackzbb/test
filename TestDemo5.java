f3d69fe571e4fa87e8646f261f9cb0eec8d24431
Jackzbb <zbb010806>
2023-04-10 16:04:32 +0800

    测试使用

---
+++ TestDemo5.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User:jack
+ * Date:2023-03-28
+ * Time:13:35
+ */
+public class TestDemo5 {
+    public static void main(String[] args) {
+        System.out.println("1000");
+    }
+}
