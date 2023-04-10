fec9e80549e8d77d3ce6ab49a5ff0255983d10dc
Jackzbb <zbb010806>
2023-04-10 17:29:27 +0800

    测试1

---
+++ TestDemo2.java
@@ -0,0 +1,10 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class TestDemo2 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("abcd");
+        }
+    }
+}
